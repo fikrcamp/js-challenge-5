39dfafc956c099732c6b377dde48d9f3df9a2963
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-16 12:13:48 +0300

    solved calculate supply --js-challenge-5

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const calculateSupply = (age,amount) =>{
+  let supply = age * amount;
+  console.log(`You will need ${supply} to last you until the old age of ${age}`);
+}
+
+calculateSupply(52,2);
+calculateSupply(20,10);
+calculateSupply(23,6);
