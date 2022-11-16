ae0cda31a35402dc7cd1025b447b7cdc46f6400c
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-16 23:52:35 +0300

    js-challenge-5

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello");
+function calculate_supply(age,amount_per_day){
+    return age * amount_per_day * 365
+}
+const max_age = calculate_supply(52,2)
+const max_age2 = calculate_supply(64,2)
+const max_age3 = calculate_supply(58,2)
+console.log(`you will need ${max_age} snack to last you until the old age of your life`);
+console.log(`you will need ${max_age2} snack to last you until the old age of your life`);
+console.log(`you will need ${max_age3} snack to last you until the old age of your life`);
+
+
